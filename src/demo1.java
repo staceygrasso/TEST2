38e46080791727683d80a93e16a27c82be412a2e
STACEY-XPS\stacey <sgrasso@ccsnh.edu>
2020-01-06 15:38:07 -0500

    148test

---
+++ src/demo1.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class demo1 {
+
+    public static void main (String args[]) {
+        int age;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your age");
+        age = in.nextInt();
+        System.out.println("current age is" + age);
+
+        System.out.println("You will be" + (age+1)  );
+
+    }
+}
+
